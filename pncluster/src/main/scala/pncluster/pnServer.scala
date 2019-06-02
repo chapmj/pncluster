e9b29f55093ab2a543d0cce10953398ff4f7d0b1
Marc Chapman <chapmjprivate+github@gmail.com>
2019-06-02 14:00:44 -0500

    added cluster refs

--- pncluster/src/main/scala/pncluster/pnServer.scala
+++ pncluster/src/main/scala/pncluster/pnServer.scala
@@ -1,16 +1,36 @@
 package pncluster
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.{ActorSystem, Props, Actor}
 import com.typesafe.config.ConfigFactory
+import akka.cluster._
+import akka.cluster.ClusterEvent._
 
 object startServer extends App {
-// sbt ./runMain ProperNameFinderApp3.startServer
+// Starts an actor system with the "server" configuration
+// sbt ./runMain pncluster.startServer
 
 	val config = 
 		args.length match { 
 			case 1 => ConfigFactory.load.getConfig(args(0))
-			case _ => ConfigFactory.load.getConfig("server1")
+			case _ => ConfigFactory.load.getConfig("server")
 		}
 
         val system = ActorSystem("ProperNamesClusterApp", config) 
+
+	//Create an actor to handle cluster messages
+	system.actorOf(Props[ClusterActor])
+}
+
+class ClusterActor extends Actor {
+/*
+ * [Citation]
+ * https://medium.com/@diego_pacheco/running-multi-nodes-akka-cluster-on-docker-24699a246c28
+*/
+	override def preStart = Cluster(context.system).subscribe(self, classOf[ClusterDomainEvent])
+
+	def receive = {
+		case MemberUp(member) => println("member up")
+		case event => println(s"event ${event.toString}")
+
+	}
 }
